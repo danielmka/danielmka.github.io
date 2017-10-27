d301bc04b87acc7b4971caa57cfb3f99dd974d90
danielkamhi <danielkamhi@gmail.com>
2017-10-27 15:40:05 -0400

    all

---
+++ js/new-age.js
@@ -0,0 +1,60 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 48)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 54
+  });
+    $( "form" ).submit(function(e) {
+         console.log(e, 'eeeee')
+         e.preventDefault();
+         var email = $('#email').val()
+
+        $.ajax({
+               type: 'POST',
+               url: 'https://hooks.zapier.com/hooks/catch/434976/r2r3yw/',
+               data: JSON.stringify({email: email}), 
+               success: function(data){
+                console.log('success')
+                $('.email').val(' ')
+                $("#thankYou").css("display","block")
+               },
+               error: function(err){
+                console.log('error')
+               }
+            })
+    });
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+})(jQuery); // End of use strict
